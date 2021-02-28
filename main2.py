55466c1eb496eefd9413bc4316376b88784fe2ef
ZiksthemW <47642797+ZiksthemW@users.noreply.github.com>
2021-02-28 19:41:08 +0300

    Sevdalısı olursa diye v2.

---
+++ main2.py
@@ -0,0 +1,25 @@
+def yaz():
+    yazi = str(input("Yazı: "))
+    sure = float(input("Kaç saniye beklenecek: "))
+    sayi = 0
+    ilk = 1
+    time.sleep(1)
+    try:
+        while True:
+            klavye.press('up')
+            klavye.typewrite(yazi[sayi])
+            klavye.press('enter')
+            sayi += 1
+            time.sleep(sure)
+            if yazi[sayi] == " ":
+                klavye.press('up')
+                klavye.typewrite(yazi[sayi])
+                sayi += 1
+                klavye.typewrite(yazi[sayi])
+                klavye.press('enter')
+                sayi += 1
+                time.sleep(sure)
+    except(IndexError):
+        print("Başarıyla yazı gönderimi tamamlandı.")
+
+yaz()
